1e11e7ee263c99d6d9c66b687f7d1db2bc194cb4
angie.lumezeanu <angie.lumezeanu@gmail.com>
2018-05-16 11:32:58 +0300

    production

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   resources :coordinates
   resources :counties
   get 'home/index'
-  root 'home#index'
+  root 'sectors#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
